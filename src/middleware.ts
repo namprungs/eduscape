60ada2e43cc75de526f273c7bed0093e2e456a2d
namprungs <6633168021@student.chula.ac.th>
2025-05-06 23:31:08 +0700

    sukT

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,21 @@
-export { default } from 'next-auth/middleware'
+import { getToken } from "next-auth/jwt"
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
 
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+
+  // ถ้าไม่มี token (ยังไม่ได้ login)
+  if (!token) {
+    const loginUrl = new URL("/login", req.url)
+    return NextResponse.redirect(loginUrl)
+  }
+
+  // ถ้ามี session แล้ว → ไปต่อได้
+  return NextResponse.next()
+}
+
+// กำหนดเฉพาะ path ที่ต้องการป้องกัน
 export const config = {
-    matcher: ["/games","/stats"]
+  matcher: ["/games", "/stats"],
 }
